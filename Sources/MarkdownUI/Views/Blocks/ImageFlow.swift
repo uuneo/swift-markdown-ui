483738a0d786f38614f594118eb3430988418c81
uuneo <uuneo@qq.com>
2025-03-30 10:44:13 +0800

    add image frame

--- Sources/MarkdownUI/Views/Blocks/ImageFlow.swift
+++ Sources/MarkdownUI/Views/Blocks/ImageFlow.swift
@@ -18,6 +18,8 @@ struct ImageFlow: View {
           switch item.value {
           case .image(let data):
             ImageView(data: data)
+                  .scaledToFit()
+                  .frame(width: 100)
           case .lineBreak:
             Spacer()
           }
